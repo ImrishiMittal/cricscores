49f8e5fe1028ef54bce484f9458f7c4c8b40a172
Rishi Mitttal <rishi11mital@gmail.com>
2026-01-30 22:50:55 +0530

    Designed Landing Page

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import LandingPage from './Pages/LandingPage'
 
 function App() {
 
   return (
     <>
-      <h1>CRICSCORERS</h1>
+      <LandingPage/>
     </>
   )
 }
--- Frontend/vite-project/src/Pages/LandingPage.jsx
+++ Frontend/vite-project/src/Pages/LandingPage.jsx
@@ -1,7 +1,26 @@
+import styles from "./LandingPage.module.css";
+import logo from "../assets/logo.png";
 
-function LandingPage(){
-    <>
-    <h1>LandingPage</h1>
-    </>
+function LandingPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.rightSection}>
+        <img src={logo} alt="CricScores Logo" />
+      </div>
+      <div className={styles.leftSection}>
+        <h1 className={styles.title}>
+          Cric<span>Scores</span>
+        </h1>
+
+        <p className={styles.tagline}><i>Score cricket matches like a pro</i></p>
+
+        <div className={styles.authLinks}>
+          <button className={styles.login}><i className="fa-solid fa-right-to-bracket"></i><b> Log In</b></button>
+          <button className={styles.signup}><i className="fa-solid fa-user-plus"></i><b> Sign Up</b></button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default LandingPage;
