63c22a4ea12a2a6207bddaacf8373ae3ff68e961
Rishi Mitttal <rishi11mital@gmail.com>
2026-02-04 16:28:02 +0530

    Improved the wicket fallen UI

--- Frontend/vite-project/src/Components/Scoring/OverBalls.jsx
+++ Frontend/vite-project/src/Components/Scoring/OverBalls.jsx
@@ -3,9 +3,21 @@ import styles from "./scoring.module.css";
 function OverBalls({ history }) {
   return (
     <div className={styles.overBalls}>
-      {history.slice(-6).map((b, i) => (
-        <div key={i} className={styles.ball}>{b.runs}</div>
-      ))}
+      {history.slice(-6).map((ball, i) => {
+        let display;
+
+        if (ball.type === "W") display = "W";
+        else display = ball.runs;
+
+        return (
+          <div
+            key={i}
+            className={`${styles.ball} ${ball.type === "W" ? styles.wicketBall : ""}`}
+          >
+            {display}
+          </div>
+        );
+      })}
     </div>
   );
 }
