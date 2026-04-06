7298bcf0c7d90206a8f6a83db0da37db43fb1d5d
Rishi Mitttal <rishi11mital@gmail.com>
2026-04-06 20:11:24 +0530

    User auth with JWT, register and login API

--- Backend/server.js
+++ Backend/server.js
@@ -8,22 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ADD THIS LINE ↓
 const playerRoutes = require("./routes/players");
+const authRoutes = require("./routes/auth"); // ADD THIS
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ Connection failed:", err));
 
-// Test route
 app.get("/", (req, res) => {
   res.json({ message: "API working ✅" });
 });
 
-// ADD THIS LINE ↓
 app.use("/api/players", playerRoutes);
+app.use("/api/auth", authRoutes); // ADD THIS
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
